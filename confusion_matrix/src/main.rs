f2626dc4c136e27ff98de19dbf8ae50569efb929
Kaonan Micadei <6026964+kaosmicadei@users.noreply.github.com>
2025-04-01 21:36:02 +0200

    improve reorder

--- confusion_matrix/src/main.rs
+++ confusion_matrix/src/main.rs
@@ -9,19 +9,23 @@ fn main() {
   let data = Array1::from_iter(1..=8);
   let m = array![[1, 2], [3, 4]];
 
-  let mut data = data;
+  let mut data = data.to_shape(((2, 4), Order::ColumnMajor)).unwrap();
 
   for i in 0..3 {
-    let order: [usize; 8] = core::array::from_fn(|v| bit_swap0(i, v));
+    // let order: [usize; 8] = core::array::from_fn(|v| bit_swap0(i, v));
+    let order: Vec<_> = (0..8).map(|v| bit_swap0(i, v)).collect();
 
-    let view = data.select(Axis(0), &order);
-    let binding = view.to_shape(((2, 4), Order::ColumnMajor)).unwrap();
-    let binding1 = m.dot(&binding);
+    let reordered = data
+      .flatten_with_order(Order::ColumnMajor)
+      .select(Axis(0), &order);
 
-    let major = if i < 2 { Order::ColumnMajor } else { Order::RowMajor };
-    data = binding1.flatten_with_order(major).into_owned();
+    let as_matirx = reordered
+      .to_shape(((2, 4), Order::ColumnMajor))
+      .unwrap();
+
+    data = m.dot(&as_matirx).into();
   }
 
-  println!("{}", data);
+  println!("{}", data.flatten());
   // [153, 351, 345, 791, 333, 763, 749, 1715]
 }
