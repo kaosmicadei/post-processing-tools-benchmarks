2324824654f199adabd929dabfd3804d9f6b69f8
Kaonan Micadei <6026964+kaosmicadei@users.noreply.github.com>
2025-04-04 00:26:10 +0200

    Vanilla Rust almost as fast as Numpy.
    * Adding BLAS crate is not helping at the moment.

---
+++ confusion_matrix/src/core.rs
@@ -0,0 +1,55 @@
+use ndarray::{Array1, Array2, ArrayView, Order};
+
+fn bit_swap0(idx: usize, value: usize) -> usize {
+  let x = (value ^ (value >> idx)) & 1;
+  value ^ ((x << idx) | x)
+}
+
+pub fn apply(m: &Array2<f32>, data: &Array1<f32>) -> Array1<f32> {
+  assert!(data.len().is_power_of_two(), "data must have 2^N elements");
+  assert_eq!(m.dim(), (2, 2), "m must be a 2x2 matrix.");
+
+  let rank = (data.len() as f32).log2() as usize;
+  let half_len = data.len() >> 1;
+
+  let mut res = data.to_shape((half_len, 2)).unwrap();
+  let mut tmp = res.clone();
+
+  for i in 0..rank {
+    // Fast "flatten": access the raw position through slice.
+    let raw_view = res.as_slice()  // WARNING! Only works with row major!
+      .unwrap();
+    let raw_tmp = tmp.as_slice_mut().unwrap();
+
+    raw_tmp
+        .iter_mut()
+        .enumerate()
+        .for_each(|(j, v)| *v = raw_view[bit_swap0(i, j)]);
+
+    let reordered = ArrayView::from_shape((half_len, 2), &raw_tmp).unwrap();
+    res.assign(&reordered.dot(m));
+  }
+
+  // Fast flatten transposed.
+  res.flatten_with_order(Order::ColumnMajor).into_owned()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use ndarray::array;
+
+  #[test]
+  fn test_bitswap() {
+    assert_eq!(bit_swap0(2, 0b0110), 0b0011);
+  }
+
+  #[test]
+  fn test_multiply_m() {
+    let data = Array1::from_iter((1..=8).map(|i| i as f32));
+    let m = array![[1.0, 3.0], [2.0, 4.0]];
+    let result = apply(&m, &data);
+    let target = array![153.0, 351.0, 345.0, 791.0, 333.0, 763.0, 749.0, 1715.0];
+    assert_eq!(result, target);
+  }
+}
---
+++ confusion_matrix/src/pybindings.rs
@@ -0,0 +1,16 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+mod confusion_matrix {
+  use super::*;
+  use crate::core;
+  use numpy::{PyArray1, PyReadonlyArray1, PyReadonlyArray2, IntoPyArray};
+  
+  #[pyfunction]
+  pub fn apply(py: Python<'_>, m: PyReadonlyArray2<f32>, data: PyReadonlyArray1<f32>) -> PyResult<Py<PyArray1<f32>>> {
+    let mm = m.as_array().to_owned();
+    let dd = data.as_array().to_owned();
+    let res = core::apply(&mm, &dd).into_pyarray(py).unbind();
+    Ok(res)
+  }  
+}
